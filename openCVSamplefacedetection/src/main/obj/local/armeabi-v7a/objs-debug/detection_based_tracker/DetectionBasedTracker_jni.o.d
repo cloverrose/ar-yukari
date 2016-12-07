5e1a9552a7141f0b96ae6e015f3a339668c3ba5a
cloverrose <clover.plus.rose@gmail.com>
2016-12-07 21:44:28 +0900

    first commit

---
+++ openCVSamplefacedetection/src/main/obj/local/armeabi-v7a/objs-debug/detection_based_tracker/DetectionBasedTracker_jni.o.d
@@ -0,0 +1,125 @@
+/Users/kenji/Desktop/face-detection/openCVSamplefacedetection/src/main/obj/local/armeabi-v7a/objs-debug/detection_based_tracker/DetectionBasedTracker_jni.o: \
+  /Users/kenji/Desktop/face-detection/openCVSamplefacedetection/src/main/jni/DetectionBasedTracker_jni.cpp \
+  /Users/kenji/Desktop/face-detection/openCVSamplefacedetection/src/main/jni/DetectionBasedTracker_jni.h \
+  /Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp \
+  /Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core.hpp \
+  /Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvdef.h \
+  /Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/hal/interface.h \
+  /Users/kenji/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstddef \
+  /Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp \
+  /Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/base.hpp \
+  /Users/kenji/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/climits \
+  /Users/kenji/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm \
+  /Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.hpp \
+  /Users/kenji/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstring \
+  /Users/kenji/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cctype \
+  /Users/kenji/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/string \
+  /Users/kenji/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstdlib \
+  /Users/kenji/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cmath \
+  /Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/ptr.inl.hpp \
+  /Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/neon_utils.hpp \
+  /Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/traits.hpp \
+  /Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/matx.hpp \
+  /Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/saturate.hpp \
+  /Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/fast_math.hpp \
+  /Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types.hpp \
+  /Users/kenji/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cfloat \
+  /Users/kenji/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/vector \
+  /Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp \
+  /Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/bufferpool.hpp \
+  /Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.inl.hpp \
+  /Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/persistence.hpp \
+  /Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp \
+  /Users/kenji/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstdio \
+  /Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.inl.hpp \
+  /Users/kenji/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/complex \
+  /Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/utility.hpp \
+  /Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h \
+  /Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h \
+  /Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/optim.hpp \
+  /Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect.hpp \
+  /Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect/detection_based_tracker.hpp \
+  /Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect/objdetect_c.h \
+  /Users/kenji/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/deque
+
+/Users/kenji/Desktop/face-detection/openCVSamplefacedetection/src/main/jni/DetectionBasedTracker_jni.h:
+
+/Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp:
+
+/Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core.hpp:
+
+/Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvdef.h:
+
+/Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/hal/interface.h:
+
+/Users/kenji/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstddef:
+
+/Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp:
+
+/Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/base.hpp:
+
+/Users/kenji/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/climits:
+
+/Users/kenji/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm:
+
+/Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.hpp:
+
+/Users/kenji/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstring:
+
+/Users/kenji/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cctype:
+
+/Users/kenji/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/string:
+
+/Users/kenji/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstdlib:
+
+/Users/kenji/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cmath:
+
+/Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/ptr.inl.hpp:
+
+/Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/neon_utils.hpp:
+
+/Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/traits.hpp:
+
+/Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/matx.hpp:
+
+/Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/saturate.hpp:
+
+/Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/fast_math.hpp:
+
+/Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types.hpp:
+
+/Users/kenji/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cfloat:
+
+/Users/kenji/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/vector:
+
+/Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp:
+
+/Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/bufferpool.hpp:
+
+/Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.inl.hpp:
+
+/Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/persistence.hpp:
+
+/Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp:
+
+/Users/kenji/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstdio:
+
+/Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.inl.hpp:
+
+/Users/kenji/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/complex:
+
+/Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/utility.hpp:
+
+/Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h:
+
+/Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h:
+
+/Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/optim.hpp:
+
+/Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect.hpp:
+
+/Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect/detection_based_tracker.hpp:
+
+/Users/kenji/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect/objdetect_c.h:
+
+/Users/kenji/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/deque:
